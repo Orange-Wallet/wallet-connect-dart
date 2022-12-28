185b2dd8a026b76d4b894c74db416cec9f2e5180
Sterling Long <sterling.long@lztek.io>
2022-12-27 20:40:22 -0700

    Signing API is working

--- test/sign_api/sign_client_test.dart
+++ test/sign_api/sign_client_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:wallet_connect_v2/apis/core/core.dart';
-import 'package:wallet_connect_v2/apis/core/i_core.dart';
 import 'package:wallet_connect_v2/apis/core/pairing/pairing_models.dart';
 import 'package:wallet_connect_v2/apis/models/models.dart';
 import 'package:wallet_connect_v2/apis/signing_api/i_sign_client.dart';
@@ -15,12 +14,6 @@ import 'sign_client_helpers.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  const TEST_PUB_KEY =
-      '9088c381b2022c6311d9b4738e221029ff4b8f3c13860a795c960eac043e7d28';
-  const TEST_PRIV_KEY =
-      'f24230adbb096e81f4a2a06450c206cafaf49dc6a60daf25d09e05c011e47ed2';
-  const TEST_TOPIC = 'abc123';
-  const TEST_MESSAGE = 'swagmaster';
   const TEST_RELAY_URL = 'ws://0.0.0.0:5555';
   const TEST_PROJECT_ID = '7e984f90b95f0236d3c12d791537f233';
 
@@ -231,7 +224,7 @@ void main() {
               []),
         };
 
-        final updateResult = await clientA.update(
+        await clientA.update(
           UpdateParams(
             sessionATopic,
             WcSessionUpdateRequest(namespacesAfter),
