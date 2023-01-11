a3ae88adfa5044ae3ccde874207dcccee4f9c1f4
Sterling Long <sterling.long@lztek.io>
2023-01-11 08:32:31 -1000

    Updated changelog, removed shared pref storage

--- lib/apis/core/store/get_storage_store.dart
+++ lib/apis/core/store/get_storage_store.dart
@@ -1,5 +1,4 @@
 import 'package:get_storage/get_storage.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wallet_connect_v2/apis/core/store/i_store.dart';
 import 'package:wallet_connect_v2/apis/utils/constants.dart';
 import 'package:wallet_connect_v2/apis/utils/errors.dart';
